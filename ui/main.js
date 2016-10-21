5e6f0d63f797d410efbd3139c59e09602baebb6b
shubhirg <shubhi.rg@gmail.com>
2016-10-21 15:59:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ submit.onclick = function(){
               comments = JSON.parse(comments);
               var list = '';
               
-              for(var i = 0; i < names.length; i++){
+              for(var i = 0; i < comments.length; i++){
                   list += '<li>' + comments[i] + '</li>';
               }
               
